7fa382f61afb1563d1519dd1848de88fc547dd51
Douglas <doug.fidelis@gmail.com>
2022-08-29 09:17:46 -0300

    created repositories

---
+++ Data/Repositories/BaseRepository.cs
@@ -0,0 +1,37 @@
+﻿using Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Repositories
+{
+    public class BaseRepository<T> : IRepository<T> where T : BaseModel
+    {
+        public string Create(T model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Update(T model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
