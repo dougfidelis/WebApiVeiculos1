3f5a65a0dd5458cce8f697986eb640ae26c73233
Douglas <doug.fidelis@gmail.com>
2022-08-29 10:51:09 -0300

    Revert "finished CarroRepository"
    This reverts commit 600387b56df46b5dba944259a24a78beed4190f3.

--- WebApiVeiculos1/Controllers/CarroController.cs
+++ WebApiVeiculos1/Controllers/CarroController.cs
@@ -36,17 +36,5 @@ namespace WebApiVeiculos1.Controllers
             return  carroRepository.GetById(id);
         }
 
-        [HttpPut]
-        public string Update(Carro model)
-        {
-            return carroRepository.Update(model);
-        }
-
-        [HttpDelete]
-        public string Delete(int id)
-        {
-            return carroRepository.Delete(id);
-        }
-
     }
 }
