900bb7a5e88074e37420d789a5d5d7415db565f7
Vinicius Marcus <vinimarcus41@gmail.com>
2022-08-29 09:46:50 -0300

    added dependecies

---
+++ Data/Context/MyContext.cs
@@ -0,0 +1,21 @@
+﻿using Data.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Context
+{
+    public class MyContext : DbContext
+    {
+        public DbSet<Carro> Carro{ get; set; }
+        public DbSet<Moto> Moto{ get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("host=localhost; Port=5540; Password=123456; UserId=postgres; Database=postgres");
+        }
+    }
+}
