3e4ad821b7ea18be88f559d502440b8365c1a9f0
Vinicius Marcus <vinimarcus41@gmail.com>
2022-08-29 10:16:13 -0300

    Creating controllers

---
+++ Data/Migrations/20220829130446_CreatingInitialDatabase.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    public partial class CreatingInitialDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Carro",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nome = table.Column<string>(type: "text", nullable: false),
+                    Marca = table.Column<string>(type: "text", nullable: false),
+                    Modelo = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Carro", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Moto",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nome = table.Column<string>(type: "text", nullable: false),
+                    Marca = table.Column<string>(type: "text", nullable: false),
+                    Modelo = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Moto", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Carro");
+
+            migrationBuilder.DropTable(
+                name: "Moto");
+        }
+    }
+}
---
+++ WebApiVeiculos1/Controllers/MotoController.cs
@@ -0,0 +1,33 @@
+﻿using Data.Model;
+using Data.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiVeiculos1.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MotoController : ControllerBase
+    {
+
+        MotoRepository motoRepo;
+
+        public MotoController()
+        {
+            this.motoRepo = new MotoRepository();
+        }
+
+        [HttpGet]
+        public List<Moto> GetAll ()
+        {
+            return motoRepo.GetAll();
+        }
+
+        [HttpPost]
+        public string Post(Moto model)
+        {
+           return motoRepo.Create(model);
+           
+        }
+        
+    }
+}
